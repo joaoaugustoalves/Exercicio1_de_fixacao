255381167ba8e0a7f24610b5295baa3978a9f360
FIC <FIC@info.local>
2020-12-07 16:43:28 -0300

    ex1 concluido

---
+++ exercicio_1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace exercicio_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.WriteLine();
+            Console.WriteLine("Qual o ano atual ? ");
+            int anoatual = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            Console.WriteLine("Em qual ano nasceu? ");
+            int anonascimento = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            int idade = anoatual - anonascimento;
+
+            if (idade >= 18) {Console.WriteLine("Você pode votar este ano ! ");} 
+            
+            else {Console.WriteLine("Você não pode votar este ano ! ");}
+        }
+    }
+}
